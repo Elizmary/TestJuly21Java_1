63ad18a0d0c8fbdc61a2ee9e3df9e379cde99ab9
Elizabeth Mary <elizabethjems@gmail.com>
2021-08-31 11:51:52 +0530

    Uploading Project

---
+++ src/p1/Demo.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		String name="Mary";
+		System.out.println(name);
+		System.out.println("Welcome to the class Demo");	
+		
+	}
+
+}
---
+++ src/p1/Test.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class Test {
+	public static void main(String[] args) {
+		String name="Elizabeth";
+		System.out.println(name);
+		System.out.println("Welcome to the class Test");	
+		
+	}
+
+}
