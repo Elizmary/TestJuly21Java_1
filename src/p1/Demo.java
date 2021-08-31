de22ee33ba17dc70d9f5a063836a03dc0be0cdde
Elizabeth Mary <elizabethjems@gmail.com>
2021-08-31 13:00:23 +0530

    Branch B Changes

--- src/p1/Demo.java
+++ src/p1/Demo.java
@@ -3,9 +3,10 @@ package p1;
 public class Demo {
 	
 	public static void main(String[] args) {
-		String name="Mary";
+		String name="Mary456";
 		System.out.println(name);
-		System.out.println("Welcome to the class Demo");	
+		System.out.println("Welcome to the class Demo");
+		System.out.println("New line for Branch B");
 		
 	}
 
